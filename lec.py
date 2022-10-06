c58c9c7353c3e412544b7521911b35b3d0ba752d
suzankarunga <suzankarunga@gmail.com>
2022-10-06 04:16:39 -0900

    lec

---
+++ lec.py
@@ -0,0 +1,8 @@
+# rRegestration form
+print("Hello BSIT")
+name=str(input("Your full name"))
+print(f"Hello {name}")
+print(f"Your{name} is" ,len(name) ,"long")
+Age=int(input("pliz input ur age"))
+Newage=Age+1
+print("age wil be" ,Newage)
