3d2b78949f62f8819dd2d45729d1967fb08aa912
suzankarunga <suzankarunga@gmail.com>
2022-10-20 04:35:54 -0900

    ASSIGNMENT2

--- Assignment/Assignment 1.py
+++ Assignment/Assignment 1.py
@@ -3,10 +3,10 @@ for i in range(1,11):
          print(i)
          i=1
          x=10
-         while i <=x:
-            if i % 2 == 0:
-                print (i,end ="")
-                 i=i+1
+while i <=x:
+  if i % 2 == 0:
+     print (i,end ="")
+     i=i+1
 #   while loop that counts from 0 to 10:
 
 
