cbe85b645c443477f32c4a4e22797cdf90f36e6e
suzankarunga <93705652+suzankarunga@users.noreply.github.com>
2022-10-27 05:18:27 -0900

    Add files via upload

---
+++ F.sring.py
@@ -0,0 +1,8 @@
+name ="hen"
+age = 40
+gender = "male "
+# with out using f string
+print("My name is "+name+" and my age is "+str(age) +"and i am "+gender)
+
+# using "f" string
+print(f"my name is{name} , and my age is {age} and i am {gender}")
